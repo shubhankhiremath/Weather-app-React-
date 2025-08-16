65e6ca33175bbf12465afdfbd4518d50bb134a1a
shubhank hiremath <shubhankhiremath@gmail.com>
2025-08-16 11:07:25 +0530

    Refactor searchBox.jsx and update weather API usage

--- src/weatherApp.jsx
+++ src/weatherApp.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import SearchBox from './searchBox.jsx';
 import { useState } from 'react';
 import InfoBox from './InfoBox';
@@ -26,4 +27,38 @@ export default function WeatherApp() {
             <InfoBox info={weatherData} />
         </div>
     );
+=======
+import SearchBox from './searchBox.jsx';
+import { useState } from 'react';
+import InfoBox from './InfoBox';
+
+export default function WeatherApp() {
+
+    let [weatherData, setWeatherData] = useState({
+        City: 'kalaburagi',
+        temp: 0,
+        Humidity: 0,
+        Feels_Like: 0,
+        weather: '',
+        temp_min: 0,
+        temp_max: 0
+
+    });
+    
+
+    let updateinfo = (result) => {
+        setWeatherData(result);
+    }
+    return (
+        <div className="app-bg">
+            <div className="main-card">
+                <div className="main-flex">
+                    <h1 style={{color : "black", margin: '0px'}}>Weather App</h1>
+                    <SearchBox updateinfo={updateinfo} />
+                    <InfoBox info={weatherData} />
+                </div>
+            </div>
+        </div>
+    );
+>>>>>>> d70eebb ( changed UI)
 }
