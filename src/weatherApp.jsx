2fe90c8ecc3412cd77f5d234c68f460d6b1b9837
shubhank hiremath <shubhankhiremath@gmail.com>
2025-08-16 12:08:12 +0530

    resolved all conflits

--- src/weatherApp.jsx
+++ src/weatherApp.jsx
@@ -3,34 +3,8 @@ import SearchBox from './searchBox.jsx';
 import { useState } from 'react';
 import InfoBox from './InfoBox';
 
-export default function WeatherApp() {
 
-    let [weatherData, setWeatherData] = useState({
-        City: 'kalaburagi',
-        temp: 0,
-        Humidity: 0,
-        Feels_Like: 0,
-        weather: '',
-        temp_min: 0,
-        temp_max: 0
 
-    });
-    
-
-    let updateinfo = (result) => {
-        setWeatherData(result);
-    }
-    return (
-        <div className="WeatherApp">
-            <h1 style={{color : "black", margin: '0px'}}>Weather App</h1>
-            <SearchBox updateinfo={updateinfo} />
-            <InfoBox info={weatherData} />
-        </div>
-    );
-};
-import SearchBox from './searchBox.jsx';
-import { useState } from 'react';
-import InfoBox from './InfoBox';
 
 export default function WeatherApp() {
 
